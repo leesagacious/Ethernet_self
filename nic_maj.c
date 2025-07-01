c3b2475ca1de12090040e3feae7069fe6c826f32
Lizhe <sensor1010@163.com>
2025-07-01 08:24:25 -0700

    main: enable the nic
    Signed-off-by: Lizhe <sensor1010@163.com>

--- nic_maj.c
+++ nic_maj.c
@@ -4,6 +4,10 @@ static int nicmaj_probe(struct pci_dev *pdev,
 {
 	int err, pci_using_dac;
 
+	err = pci_enable_device_mem(pdev);
+	if (err)
+		return err;
+
 	pci_using_dac = 0;
 
 	err = dma_set_mask(pci_dev_to_dev(pdev), DMA_BIT_MASK(64));
